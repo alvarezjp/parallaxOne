afaea90cfa7e014777c2ed02de6d7e9f23284ebe
Darlington <6.jesus.a@gmail.com>
2024-09-24 20:30:30 -0300

    galeria desktop terminada

--- src/components/Gallery/Image.jsx
+++ src/components/Gallery/Image.jsx
@@ -1,33 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
-const ContainImage = styled.div`
+const ContainImage = styled.section`
   border: 1px solid green;
-  width: 200px;
+  width: 284px;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 const IntImg = styled.img`
   border: 1px solid yellow;
   width: 100%;
-  height: 400px;
+  height: 537px;
+`;
+
+const BoxText = styled.article`
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 8px;
 `;
 const TextBottom1 = styled.h2`
-font-size: 24px;
-font-weight: 600;
-`
+  font-size: 24px;
+  font-weight: 600;
+`;
 const TextBottom2 = styled.p`
-font-size: 16px;
-font-weight: 600;
-`
+  font-size: 16px;
+  font-weight: 600;
+`;
 
-const Image = ({img}) => {
-    return (
-        <ContainImage>
-            <IntImg src={img}/>
-            <TextBottom1> Titulo</TextBottom1>
-            <TextBottom2> Texto de desarrollo</TextBottom2>
-
-        </ContainImage>
+const Image = ({ img }) => {
+  return (
+    <ContainImage>
+      <IntImg src={img} />
+      <BoxText>
+        <TextBottom1> Titulo</TextBottom1>
+        <TextBottom2> Texto de desarrollo</TextBottom2>
+      </BoxText>
+    </ContainImage>
   );
 };
 
--- src/components/Gallery/Styled.jsx
+++ src/components/Gallery/Styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ContainGallery = styled.article`
   background-color: #cfdadb;
   border: 5px solid blue;
-  height: 100vh;
+  height: 120vh;
   width: 100%;
   display: flex;
   flex-direction: column;
